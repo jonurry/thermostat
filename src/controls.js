0ee5dd4cefb5c3b6381c6a3eaabb3c73d357e71f
Jon Urry <jon@urry.me>
2018-10-04 16:26:05 +0100

    connect ui controls to model

---
+++ src/controls.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  let thermostat = new Thermostat();
+
+  let decreaseTemperature = function() {
+    thermostat.down(1);
+    updateDisplay();
+  };
+
+  let displayEnergyUsage = function() {
+    $('#energy-usage').text(thermostat.energyUsage.apply(thermostat));
+  };
+
+  let displayTemperature = function() {
+    $('#current-temperature').text(thermostat.temperature + '°C');
+  };
+
+  let increaseTemperature = function() {
+    thermostat.up(1);
+    updateDisplay();
+  };
+
+  let reset = function() {
+    thermostat.reset();
+    updateDisplay();
+  };
+
+  let setPowerSave = function(value) {
+    thermostat.powerSave = value === 'on' ? true : false;
+    displayTemperature();
+    displayEnergyUsage();
+  };
+
+  let updateDisplay = function() {
+    displayTemperature();
+    displayEnergyUsage();
+  };
+
+  $('#decrease-temperature').click(decreaseTemperature);
+  $('#increase-temperature').click(increaseTemperature);
+  $('input[name=power-save]:radio').change(function() {
+    setPowerSave($("input:radio[name='power-save']:checked").val());
+  });
+  $('#reset').click(reset);
+
+  updateDisplay();
+});
