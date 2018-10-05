32819a6e145bbdc3f0bc7c14d571db1163ffb2da
Jon Urry <jon@urry.me>
2018-10-05 14:34:40 +0100

    applied eslint

--- scripts/controls.js
+++ scripts/controls.js
@@ -1,51 +1,51 @@
 $(document).ready(function() {
-  let thermostat = new Thermostat();
+	let thermostat = new Thermostat();
 
-  let decreaseTemperature = function() {
-    thermostat.down(1);
-    updateDisplay();
-  };
+	let decreaseTemperature = function() {
+		thermostat.down(1);
+		updateDisplay();
+	};
 
-  let displayEnergyUsage = function() {
-    let energyUsage = thermostat.energyUsage.apply(thermostat);
-    $('#energy-usage').text(energyUsage);
-    $('#energy-usage').attr('class', energyUsage);
-  };
+	let displayEnergyUsage = function() {
+		let energyUsage = thermostat.energyUsage.apply(thermostat);
+		$('#energy-usage').text(energyUsage);
+		$('#energy-usage').attr('class', energyUsage);
+	};
 
-  let displayTemperature = function() {
-    $('#current-temperature').text(thermostat.temperature + '°C');
-  };
+	let displayTemperature = function() {
+		$('#current-temperature').text(thermostat.temperature + '°C');
+	};
 
-  let increaseTemperature = function() {
-    thermostat.up(1);
-    updateDisplay();
-  };
+	let increaseTemperature = function() {
+		thermostat.up(1);
+		updateDisplay();
+	};
 
-  let reset = function() {
-    thermostat.reset();
-    updateDisplay();
-  };
+	let reset = function() {
+		thermostat.reset();
+		updateDisplay();
+	};
 
-  let setPowerSave = function(value) {
-    if (value === 'on') {
-      thermostat.powerSaveOn();
-    } else {
-      thermostat.powerSaveOff();
-    }
-    updateDisplay();
-  };
+	let setPowerSave = function(value) {
+		if (value === 'on') {
+			thermostat.powerSaveOn();
+		} else {
+			thermostat.powerSaveOff();
+		}
+		updateDisplay();
+	};
 
-  let updateDisplay = function() {
-    displayTemperature();
-    displayEnergyUsage();
-  };
+	let updateDisplay = function() {
+		displayTemperature();
+		displayEnergyUsage();
+	};
 
-  $('#decrease-temperature').click(decreaseTemperature);
-  $('#increase-temperature').click(increaseTemperature);
-  $('input[name=power-save]:radio').change(function() {
-    setPowerSave($("input:radio[name='power-save']:checked").val());
-  });
-  $('#reset').click(reset);
+	$('#decrease-temperature').click(decreaseTemperature);
+	$('#increase-temperature').click(increaseTemperature);
+	$('input[name=power-save]:radio').change(function() {
+		setPowerSave($('input:radio[name=\'power-save\']:checked').val());
+	});
+	$('#reset').click(reset);
 
-  updateDisplay();
+	updateDisplay();
 });
--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,93 +1,93 @@
 describe('Thermostat', function() {
-  var thermostat;
+	var thermostat;
 
-  beforeEach(function() {
-    thermostat = new Thermostat();
-  });
+	beforeEach(function() {
+		thermostat = new Thermostat();
+	});
 
-  describe('constructor', function() {
-    it('thermostat starts at 20 degrees celsius', function() {
-      expect(thermostat.temperature).toEqual(DEFAULT_TEMP);
-    });
-  });
+	describe('constructor', function() {
+		it('thermostat starts at 20 degrees celsius', function() {
+			expect(thermostat.temperature).toEqual(DEFAULT_TEMP);
+		});
+	});
 
-  describe('up', function() {
-    it('can increase temperature', function() {
-      thermostat.up(4);
-      expect(thermostat.temperature).toEqual(24);
-    });
+	describe('up', function() {
+		it('can increase temperature', function() {
+			thermostat.up(4);
+			expect(thermostat.temperature).toEqual(24);
+		});
 
-    describe('power save on', function() {
-      it('prevents temperatures above 25 when power save is on', function() {
-        thermostat.powerSaveOn();
-        thermostat.up(60);
-        expect(thermostat.temperature).toEqual(MAX_TEMP_POWER_SAVE);
-      });
-    });
+		describe('power save on', function() {
+			it('prevents temperatures above 25 when power save is on', function() {
+				thermostat.powerSaveOn();
+				thermostat.up(60);
+				expect(thermostat.temperature).toEqual(MAX_TEMP_POWER_SAVE);
+			});
+		});
 
-    describe('power save off', function() {
-      it('prevents temperatures above 32 when power save is off', function() {
-        thermostat.powerSaveOff();
-        thermostat.up(60);
-        expect(thermostat.temperature).toEqual(MAX_TEMP);
-      });
-    });
-  });
+		describe('power save off', function() {
+			it('prevents temperatures above 32 when power save is off', function() {
+				thermostat.powerSaveOff();
+				thermostat.up(60);
+				expect(thermostat.temperature).toEqual(MAX_TEMP);
+			});
+		});
+	});
 
-  describe('down', function() {
-    it('can decrease temperature', function() {
-      thermostat.down(8);
-      expect(thermostat.temperature).toEqual(12);
-    });
+	describe('down', function() {
+		it('can decrease temperature', function() {
+			thermostat.down(8);
+			expect(thermostat.temperature).toEqual(12);
+		});
 
-    it('cannot go below 10', function() {
-      thermostat.down(17);
-      expect(thermostat.temperature).toEqual(10);
-    });
-  });
+		it('cannot go below 10', function() {
+			thermostat.down(17);
+			expect(thermostat.temperature).toEqual(10);
+		});
+	});
 
-  describe('reset', function() {
-    it('reverts to 20 when reset button is pressed', function() {
-      thermostat.up(4);
-      thermostat.reset();
-      expect(thermostat.temperature).toEqual(DEFAULT_TEMP);
-    });
-  });
+	describe('reset', function() {
+		it('reverts to 20 when reset button is pressed', function() {
+			thermostat.up(4);
+			thermostat.reset();
+			expect(thermostat.temperature).toEqual(DEFAULT_TEMP);
+		});
+	});
 
-  describe('energy usage', function() {
-    it('identifies when temperature is low energy usage', function() {
-      thermostat.temperature = LOW_ENERGY - 1;
-      expect(thermostat.energyUsage()).toEqual('low-usage');
-    });
+	describe('energy usage', function() {
+		it('identifies when temperature is low energy usage', function() {
+			thermostat.temperature = LOW_ENERGY - 1;
+			expect(thermostat.energyUsage()).toEqual('low-usage');
+		});
 
-    it('identifies when temperature is medium energy usage', function() {
-      expect(thermostat.energyUsage()).toEqual('medium-usage');
-    });
+		it('identifies when temperature is medium energy usage', function() {
+			expect(thermostat.energyUsage()).toEqual('medium-usage');
+		});
 
-    it('identifies when temperature is high energy usage', function() {
-      thermostat.temperature = HIGH_ENERGY;
-      expect(thermostat.energyUsage()).toEqual('high-usage');
-    });
-  });
+		it('identifies when temperature is high energy usage', function() {
+			thermostat.temperature = HIGH_ENERGY;
+			expect(thermostat.energyUsage()).toEqual('high-usage');
+		});
+	});
 
-  describe('powerSaveOn', function() {
-    it('switches power save on', function() {
-      thermostat.powerSave = false;
-      thermostat.powerSaveOn();
-      expect(thermostat.powerSave).toEqual(true);
-    });
-    it('enforces upper limit for temperature', function() {
-      thermostat.temperature = 30;
-      thermostat.powerSaveOn();
-      expect(thermostat.temperature).toEqual(MAX_TEMP_POWER_SAVE);
-    });
-  });
+	describe('powerSaveOn', function() {
+		it('switches power save on', function() {
+			thermostat.powerSave = false;
+			thermostat.powerSaveOn();
+			expect(thermostat.powerSave).toEqual(true);
+		});
+		it('enforces upper limit for temperature', function() {
+			thermostat.temperature = 30;
+			thermostat.powerSaveOn();
+			expect(thermostat.temperature).toEqual(MAX_TEMP_POWER_SAVE);
+		});
+	});
 
-  describe('powerSaveOff', function() {
-    it('switches power save off', function() {
-      thermostat.powerSave = true;
-      thermostat.powerSaveOff();
-      expect(thermostat.powerSave).toEqual(false);
-    });
-  });
+	describe('powerSaveOff', function() {
+		it('switches power save off', function() {
+			thermostat.powerSave = true;
+			thermostat.powerSaveOff();
+			expect(thermostat.powerSave).toEqual(false);
+		});
+	});
 });
